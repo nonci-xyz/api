a1eed8d0f8f6ae9c1126db21d8aae1a487ab9ba0
sahilpabale <dev.sahilpabale@gmail.com>
2023-07-29 17:24:01 +0530

    added retryer

--- src/worker.ts
+++ src/worker.ts
@@ -3,6 +3,7 @@ import amqplib from "amqplib";
 import bs58 from "bs58";
 import config from "config";
 import prismaClient from "config/prisma";
+import { retryer } from "amqplib-binary-retry";
 
 const queue = "txs";
 
@@ -20,51 +21,57 @@ export const worker = async () => {
     await conn.close();
   });
 
-  await channel.consume(queue, async (message) => {
-    try {
-      const txItem = JSON.parse(message!.content.toString());
-      console.log("txItem", txItem);
-      const vaultKeypair = Keypair.fromSecretKey(
-        bs58.decode(config.vaultPrivateKey)
-      );
+  await channel.consume(
+    queue,
+    retryer({
+      channel,
+      consumerQueue: queue,
+      handler: async (message) => {
+        // try {
+        const txItem = JSON.parse(message!.content.toString());
+        console.log("txItem", txItem);
+        const vaultKeypair = Keypair.fromSecretKey(
+          bs58.decode(config.vaultPrivateKey),
+        );
 
-      const transaction = Transaction.from(
-        Buffer.from(txItem.signedTx, "base64")
-      );
+        const transaction = Transaction.from(
+          Buffer.from(txItem.signedTx, "base64"),
+        );
 
-      const connection = new Connection(config.rpc, "confirmed");
+        const connection = new Connection(config.rpc, "confirmed");
 
-      transaction.recentBlockhash = txItem.durableNonce.nonceValue;
-      transaction.feePayer = vaultKeypair.publicKey;
+        transaction.recentBlockhash = txItem.durableNonce.nonceValue;
+        transaction.feePayer = vaultKeypair.publicKey;
 
-      const signature = await connection.sendTransaction(transaction, [
-        vaultKeypair,
-      ]);
+        const signature = await connection.sendTransaction(transaction, [
+          vaultKeypair,
+        ]);
 
-      await connection.confirmTransaction(signature);
+        await connection.confirmTransaction(signature);
 
-      console.log("sig", signature);
+        console.log("sig", signature);
 
-      await prismaClient.transaction.update({
-        where: {
-          id: txItem.id,
-        },
-        data: {
-          isProcessed: true,
-          signature: signature,
-          durableNonce: {
-            update: {
-              isActive: false,
+        await prismaClient.transaction.update({
+          where: {
+            id: txItem.id,
+          },
+          data: {
+            isProcessed: true,
+            signature: signature,
+            durableNonce: {
+              update: {
+                isActive: false,
+              },
             },
           },
-        },
-      });
-
-      channel.ack(message!);
-    } catch (err) {
-      console.log(err);
-
-      // TODO: add it to end of queue, also can we have a way to track that if fails more than thrice, mark it as failed
-    }
-  });
+        });
+      },
+      delayFunction(attempt) {
+        if (attempt > 3) {
+          return -1;
+        }
+        return 500 * Math.pow(2, attempt);
+      },
+    }),
+  );
 };
